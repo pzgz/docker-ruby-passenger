8fb3ff4029b2651d0e932d66f466e5933261f9fa
pzgz <leon.guan@gmail.com>
2017-05-18 11:15:48 +0800

    Make sure sidekiq daemon can work

--- sidekiq.sh
+++ sidekiq.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/app/current/
-bundle exec sidekiq -e $RAILS_ENV -L log/sidekiq.log -C config/sidekiq.yml
+exec /sbin/setuser app bundle exec sidekiq -e $RAILS_ENV -C config/sidekiq.yml >>/home/app/current/log/sidekiq.log 2>&1
